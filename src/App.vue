acd7e858be4644594fbc4986c45ce3590c47c3d2
shimhaekwon <89654237+shimhaekwon@users.noreply.github.com>
2023-06-02 13:42:59 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <input type="text" v-model="num1" />
+    <input type="text" v-model="num2" />
+    <button @click="calculateSum">Calculate</button>
+    <p>Sum: {{ sum }}</p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      sum: 0,
+    };
+  },
+  methods: {
+    calculateSum() {
+      this.sum = Number(this.num1) + Number(this.num2);
+    },
+  },
+};
+</script>
